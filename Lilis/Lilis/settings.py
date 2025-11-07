48767656afadb28d670ef21248838c69f6a6c1c7
Ubuntu <ubuntu@ip-172-31-24-110.ec2.internal>
2025-11-07 17:04:07 +0000

    AWS Pull

--- Lilis/Lilis/settings.py
+++ Lilis/Lilis/settings.py
@@ -19,7 +19,7 @@ DEBUG = os.getenv('DJANGO_DEBUG', 'True').lower() == 'true'
 
 # Usar solo codear
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','ec2-35-169-91-111.compute-1.amazonaws.com','35.169.91.111']
 
 # Usar solo presentar
 #DEBUG = False
@@ -125,8 +125,11 @@ DATE_FORMAT = 'd-m-Y'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
