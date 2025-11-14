f9183f1793cbbd6df18c7028a93f04446edc3633
imVic <victor.duran14@inacapmail.cl>
2025-11-14 12:17:12 -0300

    Fixed/error al mergear con aws solucionado

--- Lilis/Lilis/settings.py
+++ Lilis/Lilis/settings.py
@@ -132,10 +132,6 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
